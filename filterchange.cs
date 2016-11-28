66dd9f53171b1a2dfc424ff624269cffd554feac
KimHyejin <wlwwlwwlw@naver.com>
2016-11-28 12:29:36 +0900

    Update filterchange.cs

--- filterchange.cs
+++ filterchange.cs
@@ -15,22 +15,19 @@ public class filterChange : MonoBehaviour
 
     void Start()
     {
-		v = transform.localPosition;
-		connect = false;
-		bonnetpermit = false;
+        v = transform.localPosition;
+	connect = false;
+	bonnetpermit = false;
 
-		newFilter = GameObject.Find("newfilter").GetComponent<filterchange>();
-		bonnetUp = GameObject.Find("Bonnet").GetComponent<BonnetUp>();
+	newFilter = GameObject.Find("newfilter").GetComponent<filterchange>();	
+	bonnetUp = GameObject.Find("Bonnet").GetComponent<BonnetUp>();
 
-		filterNewPosition = new vector3 (-9.578f, 2.184f, -99.127f);
-		filterNewRotation = new vector3 (0.4816588f, 359.7331f, 0.6789147f);
-
+	filterNewPosition = new vector3 (-9.578f, 2.184f, -99.127f);
+	filterNewRotation = new vector3 (0.4816588f, 359.7331f, 0.6789147f);
     }
 
     void Update()
     {
-       
-    
         if (newFilter.connect == true)
         {
             if ((transform.localPosition.x >= -10.0f && transform.localPosition.x <= -8.0f) && (transform.localPosition.y >= 1.0f && transform.localPosition.y <= 3.0f) && (transform.localPosition.z >= -100.0f && transform.localPosition.z <= -98.0f))
@@ -40,8 +37,8 @@ public class filterChange : MonoBehaviour
 
             if (connect == true)
             {
-				transform.localPosition = filterNewPosition;
-				transform.rotation = Quaternion.Euler(filterNewRotation);
+		transform.localPosition = filterNewPosition;
+		transform.rotation = Quaternion.Euler(filterNewRotation);
                 GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
                 bonnetpermit = true;
                 bonnetUp.bnu = false;
