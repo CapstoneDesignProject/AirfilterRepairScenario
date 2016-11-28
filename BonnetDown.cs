39e2e3f9c02af7e79b282039425716af4e6cea5b
KimHyejin <wlwwlwwlw@naver.com>
2016-11-28 15:21:34 +0900

    Update BonnetDown.cs

--- BonnetDown.cs
+++ BonnetDown.cs
@@ -1,42 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+#define MAX_CNT 20
+
 
 public class BonnetDown : MonoBehaviour {
     public int cnt, cnt2;
-    public bool;
+    public bool flag;
 
-   
+    filterchange permission;
+    BonnetUp bonnet;
+    Vector3  bonnetPosition;	
+    Vector3  bonnetOpenRotation;
+    Vector3  bonnetCloseRoatation;
+	
     // Use this for initialization
     void Start () {
         cnt = 0;
         cnt2 = 0;
-        a = true;	
+        flag = true;	
+        
+	permission = GameObject.Find("airfiltertop").GetComponent<filterchange>();     
+	bonnet = GameObject.Find("Bonnet").GetComponent<BonnetUp>();
+	bonnetPosition = new Vector3(0, -0.2f, 0);
+	bonnetOpenRotation = new Vector3(2.0f, 0, 0);
+	bonnetCloseRotation = new Vector3(-2.0f, 0, 0);
     }
     
     public void bonnetDown()
     {
-        filterchange permission = GameObject.Find("airfiltertop").GetComponent<filterchange>();
-        BonnetUp bon = GameObject.Find("Bonnet").GetComponent<BonnetUp>();
         if (permission.bonnetpermit)
         {
-            if (cnt2 < 20)
+            if (cnt2 < MAX_CNT)
             {
-                transform.Rotate(2.0f, 0, 0);
+                transform.Rotate(bonnetOpenRotation);
                 cnt2++;
             }
-            if (cnt2 ==20&&a)
+            if (cnt2 == MAX_CNT && flag)
             {
-               bon.transform.Translate(new Vector3(0, -0.2f, 0));
-               a = false;
+               bon.transform.Translate(bonnetClosePosition);
+               flag = false;
             }
         }
     }
 	
     public void BonnetUp()
     {
-        if (cnt < 20)
+        if (cnt < MAX_CNT)
         {
-            transform.Rotate(-2.0f, 0, 0);
+            transform.Rotate(bonnetCloseRotation);
             cnt++;
         }
     }
